5f13f5c2f35777766506b4bc0a0551db3fe299fc
Christian Hujer <Christian.Hujer@nelkinda.com>
2020-02-16 23:36:00 +0530

    Add round 1.

---
+++ round1-hello/hello-sbt/build.sbt
@@ -0,0 +1,5 @@
+name := "round1-hello-sbt"
+version := "0.0.1-SNAPSHOT"
+publishMavenStyle := true
+crossPaths := false
+autoScalaLibrary := false
---
+++ round1-hello/src/main/java/com/nelkinda/blog/botbt/hello/Hello.java
@@ -0,0 +1,8 @@
+package com.nelkinda.blog.botbt.hello;
+
+@SuppressWarnings("squid:S106")
+public class Hello {
+    public static void main(final String... args) {
+        System.out.println("Hello, world ☮!");
+    }
+}
